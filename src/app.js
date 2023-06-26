fbe6930b3610e9b5561300bbd8083b882975185f
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-06-26 19:57:23 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".card").classList.add(randomSuit());
+  document.querySelector(".cardNumber").innerHTML = randomNumbers();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+/*document.querySelector(".btn").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.querySelector(".card").classList.add(randomSuit());
+  document.querySelector(".cardNumber").innerHTML = randomNumbers();
+}*/
+
+// random suit
+let randomSuit = () => {
+  let suit = ["heart", "diamond", "spade", "club"];
+  let suitIndex = Math.floor(Math.random() * suit.length);
+  return suit[suitIndex];
+};
+
+// random number
+let randomNumbers = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "J",
+    "K",
+    "Q"
+  ];
+  let numbersIndex = Math.floor(Math.random() * numbers.length);
+  return numbers[numbersIndex];
 };

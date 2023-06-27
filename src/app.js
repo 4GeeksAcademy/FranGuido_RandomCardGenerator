b7533325209a32b4d431342e9f791044062aee7a
Fran Guido <114300538+francinieguido@users.noreply.github.com>
2023-06-27 01:09:53 +0000

    update

--- src/app.js
+++ src/app.js
@@ -2,15 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-/*document.getElementById("dealCard").addEventListener("click", function() {
+// click button v1
+/*document.querySelector(".dealCard").addEventListener("click", function() {
   document.querySelector(".card").classList.add(randomSuit());
   document.querySelector(".cardNumber").innerHTML = randomNumbers();
-});*/
+});
+
+//click button v2
+document.querySelector(".dealCard").onclick = function() {
+  newCard();
+};
+function newCard() {
+  document.querySelector(".card").classList.add(randomSuit());
+  document.querySelector(".cardNumber").innerHTML = randomNumbers();
+}*/
 
 window.onload = () => {
   document.querySelector(".card").classList.add(randomSuit());
   document.querySelector(".cardNumber").innerHTML = randomNumbers();
 };
+
+// timer
+
+/*setInterval(() => {
+  document.querySelector(".card").classList.add(randomSuit());
+  document.querySelector(".cardNumber").innerHTML = randomNumbers();
+}, 1000);*/
 
 // random suit
 let randomSuit = () => {
